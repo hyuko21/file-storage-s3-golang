e68e7aa89decaf395c6036ac834b3419010b27b1
Victor Ribeiro <hyukodev@gmail.com>
2025-11-04 19:53:07 +0100

    Add function to generate presigned URLs for S3 objects

--- internal/filestorage/s3_client.go
+++ internal/filestorage/s3_client.go
@@ -2,6 +2,7 @@ package filestorage
 
 import (
 	"context"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -14,4 +15,13 @@ func MakeS3Client(region string) (c *s3.Client, err error) {
 	}
 	c = s3.NewFromConfig(cfg)
 	return
+}
+
+func GeneratePresignedURL(c *s3.Client, bucket, key string, expiry time.Duration) (string, error) {
+	presignClient := s3.NewPresignClient(c)
+	req, err := presignClient.PresignGetObject(context.TODO(), &s3.GetObjectInput{
+		Bucket: &bucket,
+		Key:    &key,
+	}, s3.WithPresignExpires(expiry))
+	return req.URL, err
 }
